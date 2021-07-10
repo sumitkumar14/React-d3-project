f69d0a97cbc827d102dccc019fe51d87f94ff924
Sumit <sumitkumar14695@gmail.com>
2021-07-11 00:38:46 +0530

    minor change

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -4,7 +4,7 @@ import React, { Component } from 'react';
 class About extends Component {
   render() {
     return (
-        <div  style={{fontWeight:'bold', display:'flex',extAlign:'justify', margin:'auto', width:'50%', fontSize:'1rem'}}>
+        <div  style={{fontWeight:'bold', display:'flex',textAlign:'justify', margin:'auto', width:'50%', fontSize:'1rem'}}>
           <article>D3 stands for Data-Driven Documents. D3.js is a JavaScript library for manipulating documents based on data. D3.js is a dynamic, interactive, online data visualizations framework used in a large number of websites. D3.js is written by Mike Bostock, created as a successor to an earlier visualization toolkit called Protovis</article>
         </div>
     );
