ea2661000901004be21a798abd3125e77aad9e38
Sumit <sumitkumar14695@gmail.com>
2021-08-08 18:27:24 +0530

    bubblechart added

--- src/components/BubbleChart.js
+++ src/components/BubbleChart.js
@@ -2,40 +2,69 @@ import React, { Component } from "react";
 import * as d3 from "d3";
 class BarChart extends Component {
   componentDidMount() {
-    const data = [2, 4, 6, 8, 10, 12, 14, 16, 18];
-    this.drawBarChart(data);
+    // const data = [2, 4, 6, 8, 10, 12, 14, 16, 18];
+    this.bubbleChart();
   }
-  drawBarChart(data) {
-    const canvasHeight = 400;
-    const canvasWidth = 600;
-    const scale = 20;
-    const svgCanvas = d3
-      .select("#bar")
+  bubbleChart() {
+    var margin = { top: 10, right: 20, bottom: 30, left: 50 },
+      width = 500 - margin.left - margin.right,
+      height = 420 - margin.top - margin.bottom;
+
+    // append the svg object to the body of the page
+    var svg = d3
+      .select("#my_dataviz")
       .append("svg")
-      .attr("width", canvasWidth)
-      .attr("height", canvasHeight)
-      .style("border", "none");
-    svgCanvas
-      .selectAll("text")
-      .data(data)
-      .enter()
-      .append("text")
-      .attr("x", (dataPoint, i) => i * 45 + 10)
-      .attr("y", (dataPoint, i) => canvasHeight - dataPoint * scale - 10)
-      .text((dataPoint) => dataPoint);
-    svgCanvas
-      .selectAll("rect")
-      .data(data)
-      .enter()
-      .append("rect")
-      .attr("width", 40)
-      .attr("height", (datapoint) => datapoint * scale)
-      .attr("fill", "orange")
-      .attr("x", (datapoint, iteration) => iteration * 45)
-      .attr("y", (datapoint) => canvasHeight - datapoint * scale);
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    //Read the data
+    d3.csv(
+      "https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/4_ThreeNum.csv").then(
+      function (data) {
+        // Add X axis
+        var x = d3.scaleLinear().domain([0, 10000]).range([0, width]);
+        svg
+          .append("g")
+          .attr("transform", "translate(0," + height + ")")
+          .call(d3.axisBottom(x));
+
+        // Add Y axis
+        var y = d3.scaleLinear().domain([35, 90]).range([height, 0]);
+        svg.append("g").call(d3.axisLeft(y));
+
+        // Add a scale for bubble size
+        var z = d3.scaleLinear().domain([200000, 1310000000]).range([1, 40]);
+
+        // Add dots
+        svg
+          .append("g")
+          .selectAll("dot")
+          .data(data)
+          .enter()
+          .append("circle")
+          .attr("cx", function (d) {
+            return x(d.gdpPercap);
+          })
+          .attr("cy", function (d) {
+            return y(d.lifeExp);
+          })
+          .attr("r", function (d) {
+            return z(d.pop);
+          })
+          .style("fill", "#69b3a2")
+          .style("opacity", "0.7")
+          .attr("stroke", "black");
+      }
+    );
   }
   render() {
-    return <div style={{justifyContent:'center', display:'flex'}}><div id="bar"></div></div>;
+    return (
+      <div style={{ justifyContent: "center", display: "flex" }}>
+        <div id="my_dataviz"></div>
+      </div>
+    );
   }
 }
 export default BarChart;
